1cad25447e756ba05c7d5ebcaf5b5b52217714da
Matteo Angelini <matteo.angelini@outlook.com>
2023-03-04 10:33:44 +0000

    fix

--- app.ts
+++ app.ts
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://organo-frontend.vercel.app/"],
+    origin: ["http://localhost:5173", "https://organo-frontend.vercel.app"],
+    credentials: true,
   })
 );
 
