a607eae3bc87ddc9279af79afc668d4a24d0d3e3
Matteo Angelini <matteo.angelini@outlook.com>
2023-03-04 09:12:46 +0000

    change port

--- app.ts
+++ app.ts
@@ -26,6 +26,7 @@ app.use("/orders", orderRouter);
 app.use("/orderItems", orderItemRouter);
 app.use("/invoices", invoiceRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
